05d61d24ba0b52808522ec424913c46cafd1b73b
sami.drach <sami.drach@bluesoft-group.com>
2023-06-06 18:53:53 +0200

    creation de la navbar + importations des polices

---
+++ src/Navbar.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import {createTheme, ThemeProvider} from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const theme = createTheme({
+    custom:{
+        basketball: '#ffbd59',
+        football: '#38b6ff',
+        natation: '#cb6ce6',
+        athletisme: '#8c52ff',
+        streetworkout: '#ffde59',
+    },
+    typography:{
+        h1:{
+            fontFamily: [
+                'Arial',
+            ],
+            fontSize: 50,
+            color: '#5d3c2b'
+        },
+        navbar:{
+            fontStyle: "none"
+        }
+    }
+})
+const pages = ['Créer une party', 'Rejoindre une party', 'Ajouter un lieu', 'Modifier un lieu'];
+const settings = ['Profil', 'Déconnexion'];
+
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = useState(null);
+    const [anchorElUser, setAnchorElUser] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static" color='transparent'>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="compte de l'utilisateur"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                         >
+                            <MenuIcon/>
+                        </IconButton>
+
+                    </Box>
+                    {/* Affichage en mode responsive  */}
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href=""
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'theme.typography.h1.fontFamily',
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Sportacus
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex'}, paddingLeft:'5rem' }}>
+                        {pages.slice(0,2).map((page, index) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                <Typography
+                                    sx={{
+                                    color:theme.typography.h1.color
+
+                                }}
+
+                                    textAlign="center">
+                                    {page}
+                                </Typography>
+                            </MenuItem>
+                        ))}
+
+                            <Typography
+                                textAlign="center"
+                                variant="h1"
+                                sx={{
+                                    mr: 2,
+                                    display: { xs: 'none', md: 'flex' }, paddingLeft:'5rem',paddingRight:'5rem',
+                                    fontFamily: theme.typography.h1,
+                                    fontSize: theme.typography.h1,
+                                    color: theme.typography.h1,
+                                }}
+                            >
+                                Sportacus
+                            </Typography>
+
+                        {pages.slice(2).map((page, index) => (
+                            <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                <Typography
+                                sx={{
+                                    color:theme.typography.h1.color
+                                }}
+                                >
+                                    {page}
+                                </Typography>
+                            </MenuItem>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Paramètres utilisateur">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Profil pic">
+                                    <AccountCircleIcon/>
+                                </Avatar>
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+
+export default ResponsiveAppBar;
