61c24d10fb60f565812872cc73f92dabde3ce291
sami.drach <sami.drach@bluesoft-group.com>
2023-07-12 15:37:39 +0200

    Hotfixes for Link in Navbar.js component

--- src/pages/Admin/AdminRouter.js
+++ src/pages/Admin/AdminRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {AdminLayout, Dashboard} from "../Admin";
-import {User, UserAdd, UserEdit} from "../Admin/User"
+import {Users, UserAdd, UserEdit} from "../Admin/User"
 import Error from "../../_utils/Error";
 import {UserDelete} from "./User";
 
@@ -12,7 +12,7 @@ const AdminRouter = () => {
                 <Route element={<AdminLayout/>}>
                     <Route path="dashboard" element={<Dashboard/>}/>
                     <Route path="user">
-                        <Route path="index" element={<User/>}></Route>
+                        <Route path="list" element={<Users/>}></Route>
                         <Route path="add" element={<UserAdd/>}></Route>
                         <Route path="edit" element={<UserEdit/>}></Route>
                         <Route path="delete" element={<UserDelete/>}></Route>
